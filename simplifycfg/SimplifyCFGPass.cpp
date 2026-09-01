c393c6911eaf2857e274b0a66902fedc442d3e3e
divnasimsic <82512072+divnasimsic@users.noreply.github.com>
2026-09-01 13:40:38 +0200

    Cleaned up and organized comments

--- simplifycfg/SimplifyCFGPass.cpp
+++ simplifycfg/SimplifyCFGPass.cpp
@@ -15,39 +15,27 @@ namespace {
 
         SimplifyCFGPass() : FunctionPass(ID) {}
 
-        //vektor blokova za brisanje
         std::vector<BasicBlock *> BlocksToRemove;
 
-        //Empty block elimiation
-        //-trazimo blok koji samo prosledjuje kontrolu dalje
         bool isEmptyBlock(BasicBlock &BB) {
 
-            //takav blok ne sme biti entry blok
-            //getParent vraca funkciju kojoj pripada BB
             if (&BB == &BB.getParent()->getEntryBlock()) {
                 return false;
             }
 
-            //takav blok mora imati tacno jednu instrukciju
             if(BB.size() != 1) {
                 return false;
             }
 
-            //ta instrukcija mora biti branch instrukcija
-            //posto znamo da potoji jedna instrukcija, mozemo uzeti terminator
             BranchInst *BI = dyn_cast<BranchInst>(BB.getTerminator());
             if (BI == nullptr) {
                 return false;
             }
 
-            //takva Branch instrukcija mora biti uncoditional
             if (BI -> isConditional()) {
                 return false;
             }
 
-            //takav blok mora imati samo jednog predecessor-a
-            //pred_begin(&BB) daje iterator na prvog predecessor-a bloka BB.
-            //pred_end(&BB) označava kraj liste predecessor-a.
             if (std::distance(pred_begin(&BB), pred_end(&BB)) != 1) {
                 return false;
             }
@@ -55,15 +43,11 @@ namespace {
             return true;
         }
 
-        // Unreachable block elimination
-        // - blok je unreachable ako nije entry i nema predecessor-a
         bool isUnreachable(BasicBlock &BB) {
-            //entry block ne uklanjamo
             if (&BB == &BB.getParent()->getEntryBlock()) {
                 return false;
             }
 
-            //ako nema predecessora do njega se ne moze doci
             if (pred_empty(&BB)) {
                 return true;
             }
@@ -89,7 +73,7 @@ namespace {
                     Changed = true;
                 }
             }
-            // prvo uklanjamo unreachable blokove
+            
             for (BasicBlock *BB : BlocksToRemove) {
                 BB->eraseFromParent();
             }
@@ -102,20 +86,15 @@ namespace {
                 if (isEmptyBlock(BB)) {
 
                     BranchInst *BI = cast<BranchInst>(BB.getTerminator());
-                    //koristimo cast umesto dym_cast jer znamo da je terminator BranchInst
 
                     BasicBlock *Pred = *pred_begin(&BB);
                     BasicBlock *Succ = BI->getSuccessor(0);
 
-                    //UKLANJANJE EMPTY BB
-                    //nalazimo terminator od predccessor-a
                     BranchInst *PredBI = dyn_cast<BranchInst>(Pred->getTerminator());
 
                     if (PredBI == nullptr) {
                         continue;
                     }
-                    //pronalazimo onaj sukcesor pridicesora koji pokazuje na empty bb
-                    //i zamenjujemo ga sukcesorom empty basic bloka
                     for (unsigned i = 0; i < PredBI->getNumSuccessors(); i++) {
                         if (PredBI->getSuccessor(i) == &BB) {
                             PredBI->setSuccessor(i, Succ);
@@ -132,7 +111,6 @@ namespace {
             }
 
 
-            //brisanje blokova
             for (BasicBlock *BB : BlocksToRemove) {
                 BB->eraseFromParent();
             }
